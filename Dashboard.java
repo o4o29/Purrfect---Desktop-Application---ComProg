52f300f79b1ed73fb5eb5b47e8b703634423bd72
lekshii <ahermoso.a62346092@umak.edu.ph>
2024-05-22 15:11:34 +0800

    Create Dashboard.java

---
+++ Dashboard.java
@@ -0,0 +1,358 @@
+package desktopMine;
+
+import java.awt.Color;
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JScrollBar;
+import javax.swing.JTextField;
+import javax.swing.JSlider;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+import javax.swing.ImageIcon;
+
+public class Dashboard implements ActionListener{
+
+    private JFrame frame;
+    private Calendar calendar;
+    private SimpleDateFormat timeFormat;
+    private SimpleDateFormat dayFormat;
+    private SimpleDateFormat dateFormat;
+    private SimpleDateFormat dateSchedFormat;
+    private JLabel timeLabel;
+    private JLabel dayLabel;
+    private JLabel dateLabel;
+    private JLabel dateSchedLabel;
+    private String time;
+    private String day;
+    private String date;
+    private String dateSched;
+    
+    private JButton btnInformation;
+    private JButton btnAppointment;
+    private JButton btnServices;
+    private JButton btnPrescription;
+    private JButton btnBilling;
+    private JButton btnReports;
+    private JButton btnLogout;
+    private JButton btnExit;
+    private JLabel lblNewLabel_1;
+    private JTextField noAppoint;
+    private JLabel name1;
+    private JLabel time1;
+    private JLabel time2;
+    private JTextField txtName1;
+    private JTextField txtTime1;
+    private JTextField txtName2;
+    private JTextField txtTime2;
+    
+    private Information info;
+    /**
+     * Launch the application.
+     */
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                try {
+                    Dashboard window = new Dashboard();
+                    window.frame.setVisible(true);
+                    window.setTime();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+
+    /**
+     * Create the application.
+     */
+    public Dashboard() {
+        initialize();
+    }
+
+    /**
+     * Initialize the contents of the frame.
+     */
+    private void initialize() {
+    	info = new Information();
+    	
+        frame = new JFrame("Dashboard");
+        frame.getContentPane().setBackground(new Color(250,242,208));
+        frame.getContentPane().setLayout(null);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(713, 436); // Set the initial size of the frame
+        frame.setResizable(false);
+        frame.setLocationRelativeTo(null);
+
+        btnInformation = new JButton("INFORMATION");
+        btnInformation.setBackground(new Color(249,173,183));
+        btnInformation.setFont(new Font("Verdana", Font.PLAIN, 14));
+        btnInformation.setBounds(24, 177, 144, 55);
+        btnInformation.addActionListener(this);
+        btnInformation.setFocusable(false);
+        frame.getContentPane().add(btnInformation);
+
+        btnAppointment = new JButton("APPOINTMENT");
+        btnAppointment.setBackground(new Color(249,173,183));
+        btnAppointment.setFont(new Font("Verdana", Font.PLAIN, 14));
+        btnAppointment.setBounds(185, 177, 144, 55);
+        btnAppointment.addActionListener(this);
+        btnAppointment.setFocusable(false);
+        frame.getContentPane().add(btnAppointment);
+
+        btnServices = new JButton("SERVICES");
+        btnServices.setBackground(new Color(249,173,183));
+        btnServices.setFont(new Font("Verdana", Font.PLAIN, 14));
+        btnServices.setBounds(349, 177, 144, 55);
+        btnServices.addActionListener(this);
+        btnServices.setFocusable(false);
+        frame.getContentPane().add(btnServices);
+
+        btnPrescription = new JButton("PRESCRIPTION");
+        btnPrescription.setBackground(new Color(249,173,183));
+        btnPrescription.setFont(new Font("Verdana", Font.PLAIN, 14));
+        btnPrescription.setBounds(24, 254, 144, 55);
+        btnPrescription.addActionListener(this);
+        btnPrescription.setFocusable(false);
+        frame.getContentPane().add(btnPrescription);
+
+        btnBilling = new JButton("BILLING");
+        btnBilling.setBackground(new Color(249,173,183));
+        btnBilling.setFont(new Font("Verdana", Font.PLAIN, 14));
+        btnBilling.setBounds(185, 254, 144, 55);
+        btnBilling.addActionListener(this);
+        btnBilling.setFocusable(false);
+        frame.getContentPane().add(btnBilling);
+
+        btnReports = new JButton("REPORTS");
+        btnReports.setBackground(new Color(249,173,183));
+        btnReports.setFont(new Font("Verdana", Font.PLAIN, 14));
+        btnReports.setBounds(349, 254, 144, 55);
+        btnReports.addActionListener(this);
+        btnReports.setFocusable(false);
+        frame.getContentPane().add(btnReports);
+
+        calendar = Calendar.getInstance();
+        timeFormat = new SimpleDateFormat("hh:mm:ss a");
+        dayFormat = new SimpleDateFormat("EEEE");
+        dateFormat = new SimpleDateFormat("MMMM dd, yyyy");
+        dateSchedFormat = new SimpleDateFormat("MMMM dd, yyyy");
+        time = timeFormat.format(calendar.getTime());
+
+        JPanel panelHead = new JPanel();
+        panelHead.setBackground(new Color(233,129,164));
+        panelHead.setBounds(0, 0, 697, 155);
+        frame.getContentPane().add(panelHead);
+        panelHead.setLayout(null);
+        
+        ImageIcon logo = new ImageIcon("logo.png");
+        JLabel logoLabel = new JLabel();
+        logoLabel.setIcon(logo);
+        logoLabel.setBounds(22, 11, 134, 133);
+        panelHead.add(logoLabel);
+        
+        timeLabel = new JLabel();
+        timeLabel.setFont(new Font("Verdana", Font.PLAIN, 40));
+        timeLabel.setBounds(209, 68, 270, 43);
+        panelHead.add(timeLabel);
+        timeLabel.setText(time);
+        
+        dayLabel = new JLabel();
+        dayLabel.setFont(new Font("Verdana", Font.PLAIN, 20));
+        dayLabel.setBounds(197, 101, 160, 43);
+        panelHead.add(dayLabel);
+        dayLabel.setText(day);
+        
+        dateLabel = new JLabel();
+        dateLabel.setFont(new Font("Verdana", Font.PLAIN, 20));
+        dateLabel.setBounds(331, 101, 184, 43);
+        panelHead.add(dateLabel);
+        dateLabel.setText(date);
+
+        JLabel lblVcms = new JLabel("PURRFECT");
+        lblVcms.setBounds(224, 11, 254, 64);
+        panelHead.add(lblVcms);
+        lblVcms.setForeground(new Color(250,242,208));
+        lblVcms.setFont(new Font("Rockwell Extra Bold", Font.BOLD, 37));
+        
+        btnLogout = new JButton("LOG OUT");
+        btnLogout.setFont(new Font("Verdana", Font.PLAIN, 13));
+        btnLogout.setBackground(new Color(254,201,195));
+        btnLogout.setBounds(132, 338, 100, 37);
+        btnLogout.addActionListener(this);
+        btnLogout.setFocusable(false);
+        frame.getContentPane().add(btnLogout);
+        
+        btnExit = new JButton("EXIT");
+        btnExit.setFont(new Font("Verdana", Font.PLAIN, 13));
+        btnExit.setBackground(new Color(254,201,195));
+        btnExit.setBounds(291, 338, 100, 37);
+        btnExit.addActionListener(this);
+        btnExit.setFocusable(false);
+        frame.getContentPane().add(btnExit);
+        
+        JPanel panelSched = new JPanel();
+        panelSched.setBorder(new LineBorder(new Color(244,197,215), 2, true));
+        panelSched.setBackground(new Color(255, 182, 193));
+        panelSched.setBounds(509, 166, 178, 231);
+        frame.getContentPane().add(panelSched);
+        panelSched.setLayout(null);
+        
+        JLabel lblNewLabel = new JLabel("Appointment Schedule");
+        lblNewLabel.setBackground(new Color(255, 217, 236));
+        lblNewLabel.setFont(new Font("Arial", Font.PLAIN, 14));
+        lblNewLabel.setBounds(20, 11, 158, 14);
+        panelSched.add(lblNewLabel);
+        
+        dateSchedLabel = new JLabel();
+        dateSchedLabel.setBounds(41, 25, 107, 21);
+        panelSched.add(dateSchedLabel);
+        dateSchedLabel.setFont(new Font("Verdana", Font.PLAIN, 14));
+        dateSchedLabel.setText(dateSched);
+        
+        lblNewLabel_1 = new JLabel("Appointments Today: ");
+        lblNewLabel_1.setFont(new Font("Verdana", Font.PLAIN, 11));
+        lblNewLabel_1.setBounds(10, 56, 138, 14);
+        panelSched.add(lblNewLabel_1);
+        
+        noAppoint = new JTextField();
+        noAppoint.setBackground(new Color(250,242,208));
+        noAppoint.setEditable(false);
+        noAppoint.setBounds(138, 54, 30, 20);
+        panelSched.add(noAppoint);
+        noAppoint.setColumns(10);
+        
+        name1 = new JLabel("Name:");
+        name1.setFont(new Font("Verdana", Font.PLAIN, 11));
+        name1.setBounds(20, 92, 46, 14);
+        panelSched.add(name1);
+        
+        time1 = new JLabel("Time:");
+        time1.setFont(new Font("Verdana", Font.PLAIN, 11));
+        time1.setBounds(20, 119, 46, 14);
+        panelSched.add(time1);
+        
+        JLabel name2 = new JLabel("Name:");
+        name2.setFont(new Font("Verdana", Font.PLAIN, 11));
+        name2.setBounds(20, 155, 46, 14);
+        panelSched.add(name2);
+        
+        time2 = new JLabel("Time:");
+        time2.setFont(new Font("Verdana", Font.PLAIN, 11));
+        time2.setBounds(20, 180, 46, 14);
+        panelSched.add(time2);
+        
+        txtName1 = new JTextField();
+        txtName1.setBackground(new Color(250,242,208));
+        txtName1.setEditable(false);
+        txtName1.setBounds(63, 90, 105, 20);
+        panelSched.add(txtName1);
+        txtName1.setColumns(10);
+        
+        txtTime1 = new JTextField();
+        txtTime1.setBackground(new Color(250,242,208));
+        txtTime1.setEditable(false);
+        txtTime1.setColumns(10);
+        txtTime1.setBounds(63, 117, 105, 20);
+        panelSched.add(txtTime1);
+        
+        txtName2 = new JTextField();
+        txtName2.setBackground(new Color(250,242,208));
+        txtName2.setEditable(false);
+        txtName2.setColumns(10);
+        txtName2.setBounds(63, 153, 105, 20);
+        panelSched.add(txtName2);
+        
+        txtTime2 = new JTextField();
+        txtTime2.setBackground(new Color(250,242,208));
+        txtTime2.setEditable(false);
+        txtTime2.setColumns(10);
+        txtTime2.setBounds(63, 178, 105, 20);
+        panelSched.add(txtTime2);
+        
+        JScrollBar scrollBar = new JScrollBar();
+        scrollBar.setOrientation(JScrollBar.HORIZONTAL);
+        scrollBar.setBounds(83, 209, 17, 21);
+        panelSched.add(scrollBar);
+        
+        /*dateSched = new JLabel();
+        dateSched.setText((String) null);
+        dateSched.setFont(new Font("Verdana", Font.PLAIN, 14));
+        dateSched.setBounds(10, 23, 168, 29);
+        panelSched.add(dateSched);
+        */
+    }
+
+    public void setTime() {
+        Thread updateTimeThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    time = timeFormat.format(Calendar.getInstance().getTime());
+                    timeLabel.setText(time);
+                    
+                    day = dayFormat.format(Calendar.getInstance().getTime());
+                    dayLabel.setText(day);
+                    
+                    date = dateFormat.format(Calendar.getInstance().getTime());
+                    dateLabel.setText(date);
+                    
+                    dateSched = dateFormat.format(Calendar.getInstance().getTime());
+                    dateSchedLabel.setText(dateSched);
+
+                    try {
+                        Thread.sleep(1000); // Update every second
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        updateTimeThread.start();
+    }
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if(e.getSource().equals(btnInformation)) {
+			info.initialize(); 
+		}
+		
+		if(e.getSource().equals(btnAppointment)) {
+			
+		}	
+
+		if(e.getSource().equals(btnServices)) {
+			
+		}	
+
+		if(e.getSource().equals(btnPrescription)) {
+			
+		}
+
+		if(e.getSource().equals(btnBilling)) {
+			
+		}	
+
+		if(e.getSource().equals(btnReports)) {
+			
+		}	
+
+		if(e.getSource().equals(btnLogout)) {
+			
+		}	
+
+		if(e.getSource().equals(btnExit)) {
+			
+		}	
+	}
+}
