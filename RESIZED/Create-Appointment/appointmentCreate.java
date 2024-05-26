8ba6789ab32fcc1827b9825de24b967d39e5d766
lekshii <ahermoso.a62346092@umak.edu.ph>
2024-05-26 22:20:58 +0800

    Update appointmentCreate.java

--- RESIZED/Create-Appointment/appointmentCreate.java
+++ RESIZED/Create-Appointment/appointmentCreate.java
@@ -19,10 +19,11 @@ import java.awt.event.ActionEvent;
 public class appointmentCreate{
 
 	private JFrame frame;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField petID_Field;
+	private JTextField petName_Field;
+	private JTextField clientID_Field;
+	private JTextField date_Field;
+	private JTextField time_Field;
 
 	/**
 	 * Launch the application.
@@ -70,59 +71,69 @@ public class appointmentCreate{
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(255, 239, 213));
 		panel.setForeground(new Color(255, 235, 205));
-		panel.setBounds(23, 116, 297, 129);
+		panel.setBounds(23, 113, 297, 163);
 		frame.getContentPane().add(panel);
 		panel.setLayout(null);
 		
 		JLabel lblNewLabel = new JLabel("Client ID:");
-		lblNewLabel.setBounds(10, 5, 72, 31);
+		lblNewLabel.setBounds(10, 63, 72, 31);
 		panel.add(lblNewLabel);
 		lblNewLabel.setFont(new Font("Verdana", Font.PLAIN, 13));
 		
 		JLabel lblPetId = new JLabel("Pet ID:");
 		lblPetId.setFont(new Font("Verdana", Font.PLAIN, 13));
-		lblPetId.setBounds(10, 35, 59, 31);
+		lblPetId.setBounds(10, 5, 59, 31);
 		panel.add(lblPetId);
 		
 		JLabel lblDate = new JLabel("Date: ");
 		lblDate.setFont(new Font("Verdana", Font.PLAIN, 13));
-		lblDate.setBounds(10, 60, 44, 31);
+		lblDate.setBounds(10, 91, 44, 31);
 		panel.add(lblDate);
 		
 		JLabel lblPetId_1_1 = new JLabel("Time:");
 		lblPetId_1_1.setFont(new Font("Verdana", Font.PLAIN, 13));
-		lblPetId_1_1.setBounds(10, 88, 44, 31);
+		lblPetId_1_1.setBounds(10, 124, 44, 31);
 		panel.add(lblPetId_1_1);
 		
-		textField = new JTextField();
-		textField.setBounds(85, 11, 186, 22);
-		panel.add(textField);
-		textField.setColumns(10);
+		petID_Field = new JTextField();
+		petID_Field.setBounds(85, 11, 186, 22);
+		panel.add(petID_Field);
+		petID_Field.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setColumns(10);
-		textField_1.setBounds(85, 41, 186, 22);
-		panel.add(textField_1);
+		petName_Field = new JTextField();
+		petName_Field.setColumns(10);
+		petName_Field.setBounds(85, 41, 186, 22);
+		panel.add(petName_Field);
 		
-		textField_2 = new JTextField();
-		textField_2.setColumns(10);
-		textField_2.setBounds(85, 69, 186, 22);
-		panel.add(textField_2);
+		clientID_Field = new JTextField();
+		clientID_Field.setColumns(10);
+		clientID_Field.setBounds(85, 69, 186, 22);
+		panel.add(clientID_Field);
 		
-		textField_3 = new JTextField();
-		textField_3.setColumns(10);
-		textField_3.setBounds(85, 97, 186, 22);
-		panel.add(textField_3);
+		date_Field = new JTextField();
+		date_Field.setColumns(10);
+		date_Field.setBounds(85, 97, 186, 22);
+		panel.add(date_Field);
+		
+		time_Field = new JTextField();
+		time_Field.setColumns(10);
+		time_Field.setBounds(85, 130, 186, 22);
+		panel.add(time_Field);
+		
+		JLabel lblPetName = new JLabel("Pet Name:");
+		lblPetName.setFont(new Font("Verdana", Font.PLAIN, 13));
+		lblPetName.setBounds(10, 32, 72, 31);
+		panel.add(lblPetName);
 		
 		JLabel lblPetId_1_1_1 = new JLabel("Choose your Concern/ Service:");
 		lblPetId_1_1_1.setBackground(new Color(240, 240, 240));
 		lblPetId_1_1_1.setFont(new Font("Verdana", Font.PLAIN, 17));
-		lblPetId_1_1_1.setBounds(10, 271, 330, 46);
+		lblPetId_1_1_1.setBounds(10, 298, 330, 46);
 		frame.getContentPane().add(lblPetId_1_1_1);
 		
 		JTextArea textArea_1_1 = new JTextArea();
 		textArea_1_1.setBackground(new Color(255, 182, 193));
-		textArea_1_1.setBounds(0, 271, 884, 46);
+		textArea_1_1.setBounds(0, 298, 884, 46);
 		frame.getContentPane().add(textArea_1_1);
 		
 		JPanel panel_1 = new JPanel();
@@ -130,72 +141,72 @@ public class appointmentCreate{
 		panel_1.setLayout(null);
 		panel_1.setForeground(new Color(255, 235, 205));
 		panel_1.setBackground(new Color(255, 239, 213));
-		panel_1.setBounds(23, 376, 322, 94);
+		panel_1.setBounds(23, 387, 322, 94);
 		frame.getContentPane().add(panel_1);
 		
-		JCheckBox chckbxNewCheckBox = new JCheckBox("Wellness Exams (Monitor)");
-		chckbxNewCheckBox.setFont(new Font("Verdana", Font.PLAIN, 13));
-		chckbxNewCheckBox.setBackground(new Color(255, 239, 213));
-		chckbxNewCheckBox.setBounds(19, 22, 218, 23);
-		panel_1.add(chckbxNewCheckBox);
+		JCheckBox wellnessExam_Chk = new JCheckBox("Wellness Exams (Monitor)");
+		wellnessExam_Chk.setFont(new Font("Verdana", Font.PLAIN, 13));
+		wellnessExam_Chk.setBackground(new Color(255, 239, 213));
+		wellnessExam_Chk.setBounds(19, 22, 218, 23);
+		panel_1.add(wellnessExam_Chk);
 		
-		JCheckBox chckbxPreventiveCareadvice = new JCheckBox("Preventive Care \r\n(Advice and Treatment)\r\n");
-		chckbxPreventiveCareadvice.setFont(new Font("Verdana", Font.PLAIN, 13));
-		chckbxPreventiveCareadvice.setBackground(new Color(255, 239, 213));
-		chckbxPreventiveCareadvice.setBounds(19, 49, 295, 23);
-		panel_1.add(chckbxPreventiveCareadvice);
+		JCheckBox preventCare_Chk = new JCheckBox("Preventive Care \r\n(Advice and Treatment)\r\n");
+		preventCare_Chk.setFont(new Font("Verdana", Font.PLAIN, 13));
+		preventCare_Chk.setBackground(new Color(255, 239, 213));
+		preventCare_Chk.setBounds(19, 49, 295, 23);
+		panel_1.add(preventCare_Chk);
 		
 		JPanel panel_1_1 = new JPanel();
 		panel_1_1.setBorder(new TitledBorder(null, "Diagnostic", TitledBorder.LEADING, TitledBorder.TOP, null, null));
 		panel_1_1.setLayout(null);
 		panel_1_1.setForeground(new Color(255, 235, 205));
 		panel_1_1.setBackground(new Color(255, 239, 213));
-		panel_1_1.setBounds(360, 361, 230, 120);
+		panel_1_1.setBounds(355, 373, 230, 120);
 		frame.getContentPane().add(panel_1_1);
 		
-		JCheckBox chckbxLaboratoryTesting = new JCheckBox("Laboratory Testing");
-		chckbxLaboratoryTesting.setFont(new Font("Verdana", Font.PLAIN, 13));
-		chckbxLaboratoryTesting.setBackground(new Color(255, 239, 213));
-		chckbxLaboratoryTesting.setBounds(18, 22, 206, 23);
-		panel_1_1.add(chckbxLaboratoryTesting);
+		JCheckBox labTesting_Chk = new JCheckBox("Laboratory Testing");
+		labTesting_Chk.setFont(new Font("Verdana", Font.PLAIN, 13));
+		labTesting_Chk.setBackground(new Color(255, 239, 213));
+		labTesting_Chk.setBounds(18, 22, 206, 23);
+		panel_1_1.add(labTesting_Chk);
 		
-		JCheckBox chckbxImagingServices = new JCheckBox("Imaging Services");
-		chckbxImagingServices.setFont(new Font("Verdana", Font.PLAIN, 13));
-		chckbxImagingServices.setBackground(new Color(255, 239, 213));
-		chckbxImagingServices.setBounds(18, 48, 206, 23);
-		panel_1_1.add(chckbxImagingServices);
+		JCheckBox imagingServ_Chk = new JCheckBox("Imaging Services");
+		imagingServ_Chk.setFont(new Font("Verdana", Font.PLAIN, 13));
+		imagingServ_Chk.setBackground(new Color(255, 239, 213));
+		imagingServ_Chk.setBounds(18, 48, 206, 23);
+		panel_1_1.add(imagingServ_Chk);
 		
-		JCheckBox chckbxEcgAndEkg = new JCheckBox("ECG and EKG");
-		chckbxEcgAndEkg.setFont(new Font("Verdana", Font.PLAIN, 13));
-		chckbxEcgAndEkg.setBackground(new Color(255, 239, 213));
-		chckbxEcgAndEkg.setBounds(18, 74, 206, 23);
-		panel_1_1.add(chckbxEcgAndEkg);
+		JCheckBox ecgEkg_Chk = new JCheckBox("ECG and EKG");
+		ecgEkg_Chk.setFont(new Font("Verdana", Font.PLAIN, 13));
+		ecgEkg_Chk.setBackground(new Color(255, 239, 213));
+		ecgEkg_Chk.setBounds(18, 74, 206, 23);
+		panel_1_1.add(ecgEkg_Chk);
 		
 		JPanel panel_1_1_1 = new JPanel();
 		panel_1_1_1.setLayout(null);
 		panel_1_1_1.setForeground(new Color(255, 235, 205));
 		panel_1_1_1.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "Surgery", TitledBorder.LEADING, TitledBorder.TOP, null, new Color(0, 0, 0)));
 		panel_1_1_1.setBackground(new Color(255, 239, 213));
-		panel_1_1_1.setBounds(611, 361, 230, 120);
+		panel_1_1_1.setBounds(611, 373, 230, 120);
 		frame.getContentPane().add(panel_1_1_1);
 		
-		JCheckBox chckbxSpayingAndNeuturing = new JCheckBox("Spaying and Neuturing");
-		chckbxSpayingAndNeuturing.setFont(new Font("Verdana", Font.PLAIN, 13));
-		chckbxSpayingAndNeuturing.setBackground(new Color(255, 239, 213));
-		chckbxSpayingAndNeuturing.setBounds(16, 22, 188, 23);
-		panel_1_1_1.add(chckbxSpayingAndNeuturing);
+		JCheckBox spayNeut_Chk = new JCheckBox("Spaying and Neuturing");
+		spayNeut_Chk.setFont(new Font("Verdana", Font.PLAIN, 13));
+		spayNeut_Chk.setBackground(new Color(255, 239, 213));
+		spayNeut_Chk.setBounds(16, 22, 188, 23);
+		panel_1_1_1.add(spayNeut_Chk);
 		
-		JCheckBox chckbxTumorRemoval = new JCheckBox("Tumor Removal");
-		chckbxTumorRemoval.setFont(new Font("Verdana", Font.PLAIN, 13));
-		chckbxTumorRemoval.setBackground(new Color(255, 239, 213));
-		chckbxTumorRemoval.setBounds(16, 48, 188, 23);
-		panel_1_1_1.add(chckbxTumorRemoval);
+		JCheckBox tumorRemoval_Chk = new JCheckBox("Tumor Removal");
+		tumorRemoval_Chk.setFont(new Font("Verdana", Font.PLAIN, 13));
+		tumorRemoval_Chk.setBackground(new Color(255, 239, 213));
+		tumorRemoval_Chk.setBounds(16, 48, 188, 23);
+		panel_1_1_1.add(tumorRemoval_Chk);
 		
-		JCheckBox chckbxWoundRepair = new JCheckBox("Wound Repair");
-		chckbxWoundRepair.setFont(new Font("Verdana", Font.PLAIN, 13));
-		chckbxWoundRepair.setBounds(16, 76, 190, 23);
-		panel_1_1_1.add(chckbxWoundRepair);
-		chckbxWoundRepair.setBackground(new Color(255, 239, 213));
+		JCheckBox woundRepair_Chk = new JCheckBox("Wound Repair");
+		woundRepair_Chk.setFont(new Font("Verdana", Font.PLAIN, 13));
+		woundRepair_Chk.setBounds(16, 76, 190, 23);
+		panel_1_1_1.add(woundRepair_Chk);
+		woundRepair_Chk.setBackground(new Color(255, 239, 213));
 		
 		JLabel lblCompleteAppointment_1 = new JLabel("1\r\n");
 		lblCompleteAppointment_1.setForeground(Color.WHITE);
