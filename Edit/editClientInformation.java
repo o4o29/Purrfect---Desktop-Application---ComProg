16e39f183e5201b3fd271ad6df23e53b1a54678b
o4o29 <roseannetang29@gmail.com>
2024-05-28 01:40:09 +0800

    Update editClientInformation.java

--- Edit/editClientInformation.java
+++ Edit/editClientInformation.java
@@ -16,6 +16,7 @@ public class editClientInformation {
 	private JTextField LName_Field;
 	private JTextField contact_Field;
 	private JTextField emailAdd_Field;
+	private JTextField clientID_Field;
 
 	/**
 	 * Launch the application.
@@ -72,62 +73,72 @@ public class editClientInformation {
 		JPanel mainPanel = new JPanel();
 		mainPanel.setLayout(null);
 		mainPanel.setBackground(new Color(255, 182, 193));
-		mainPanel.setBounds(215, 184, 411, 293);
+		mainPanel.setBounds(215, 184, 411, 328);
 		frmEditClientInformation.getContentPane().add(mainPanel);
 		
 		JLabel lblNewLabel_2 = new JLabel("First Name:");
 		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_2.setBounds(31, 25, 76, 19);
+		lblNewLabel_2.setBounds(31, 47, 76, 19);
 		mainPanel.add(lblNewLabel_2);
 		
 		JLabel lblNewLabel_2_1 = new JLabel("Last Name:");
 		lblNewLabel_2_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_2_1.setBounds(31, 55, 96, 14);
+		lblNewLabel_2_1.setBounds(31, 80, 96, 14);
 		mainPanel.add(lblNewLabel_2_1);
 		
 		JLabel lblNewLabel_2_1_1 = new JLabel("Address:");
 		lblNewLabel_2_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_2_1_1.setBounds(31, 80, 106, 14);
+		lblNewLabel_2_1_1.setBounds(31, 113, 106, 14);
 		mainPanel.add(lblNewLabel_2_1_1);
 		
 		JLabel lblNewLabel_2_1_1_1 = new JLabel("Contact No.:");
 		lblNewLabel_2_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_2_1_1_1.setBounds(31, 157, 106, 14);
+		lblNewLabel_2_1_1_1.setBounds(31, 187, 106, 14);
 		mainPanel.add(lblNewLabel_2_1_1_1);
 		
 		JLabel lblNewLabel_2_1_1_1_1 = new JLabel("Email Address:");
 		lblNewLabel_2_1_1_1_1.setFont(new Font("Tahoma", Font.PLAIN, 15));
-		lblNewLabel_2_1_1_1_1.setBounds(31, 182, 106, 14);
+		lblNewLabel_2_1_1_1_1.setBounds(31, 213, 106, 14);
 		mainPanel.add(lblNewLabel_2_1_1_1_1);
 		
 		FName_Field = new JTextField();
 		FName_Field.setColumns(10);
-		FName_Field.setBounds(133, 26, 248, 20);
+		FName_Field.setBounds(133, 48, 248, 20);
 		mainPanel.add(FName_Field);
 		
 		LName_Field = new JTextField();
 		LName_Field.setColumns(10);
-		LName_Field.setBounds(133, 54, 248, 20);
+		LName_Field.setBounds(133, 79, 248, 20);
 		mainPanel.add(LName_Field);
 		
 		contact_Field = new JTextField();
 		contact_Field.setColumns(10);
-		contact_Field.setBounds(133, 156, 248, 20);
+		contact_Field.setBounds(133, 186, 248, 20);
 		mainPanel.add(contact_Field);
 		
 		emailAdd_Field = new JTextField();
 		emailAdd_Field.setColumns(10);
-		emailAdd_Field.setBounds(133, 181, 248, 20);
+		emailAdd_Field.setBounds(133, 212, 248, 20);
 		mainPanel.add(emailAdd_Field);
 		
 		JTextArea address_Field = new JTextArea();
-		address_Field.setBounds(133, 80, 248, 66);
+		address_Field.setBounds(133, 110, 248, 66);
 		mainPanel.add(address_Field);
 		
 		JButton submitChanges_Btn = new JButton("SUBMIT CHANGES");
 		submitChanges_Btn.setFont(new Font("Tahoma", Font.PLAIN, 13));
 		submitChanges_Btn.setBackground(new Color(255, 240, 245));
-		submitChanges_Btn.setBounds(129, 257, 150, 25);
+		submitChanges_Btn.setBounds(133, 273, 150, 25);
 		mainPanel.add(submitChanges_Btn);
+		
+		JLabel lblNewLabel_2_2 = new JLabel("CLIENT ID:");
+		lblNewLabel_2_2.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel_2_2.setBounds(31, 11, 96, 19);
+		mainPanel.add(lblNewLabel_2_2);
+		
+		clientID_Field = new JTextField();
+		clientID_Field.setColumns(10);
+		clientID_Field.setBounds(133, 12, 248, 20);
+		mainPanel.add(clientID_Field);
 	}
 }
